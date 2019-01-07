776212276c9b81ef545dc887cf1d0a359a9de5c3
zhangxiaozhou <710240507@qq.com>
2019-01-07 11:00:59 +0800

    init

---
+++ src/main/java/com/mslife/proposal/image/ImageServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mslife.proposal.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageServiceApplication.class, args);
+	}
+
+}
+
